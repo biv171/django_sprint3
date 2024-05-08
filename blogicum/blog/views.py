d57cc2e749d5d13e8f50148b94a057093d3865d9
Garry <biv171@yandex.ru>
2024-05-09 01:03:01 +0300

    Django 2th project DB

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -1,76 +1,55 @@
-from django.shortcuts import render
-from django.http import Http404
-
-
-posts = [
-    {
-        'id': 0,
-        'location': 'Остров отчаянья',
-        'date': '30 сентября 1659 года',
-        'category': 'travel',
-        'text': '''Наш корабль, застигнутый в открытом море
-                страшным штормом, потерпел крушение.
-                Весь экипаж, кроме меня, утонул; я же,
-                несчастный Робинзон Крузо, был выброшен
-                полумёртвым на берег этого проклятого острова,
-                который назвал островом Отчаяния.''',
-    },
-    {
-        'id': 1,
-        'location': 'Остров отчаянья',
-        'date': '1 октября 1659 года',
-        'category': 'not-my-day',
-        'text': '''Проснувшись поутру, я увидел, что наш корабль сняло
-                с мели приливом и пригнало гораздо ближе к берегу.
-                Это подало мне надежду, что, когда ветер стихнет,
-                мне удастся добраться до корабля и запастись едой и
-                другими необходимыми вещами. Я немного приободрился,
-                хотя печаль о погибших товарищах не покидала меня.
-                Мне всё думалось, что, останься мы на корабле, мы
-                непременно спаслись бы. Теперь из его обломков мы могли бы
-                построить баркас, на котором и выбрались бы из этого
-                гиблого места.''',
-    },
-    {
-        'id': 2,
-        'location': 'Остров отчаянья',
-        'date': '25 октября 1659 года',
-        'category': 'not-my-day',
-        'text': '''Всю ночь и весь день шёл дождь и дул сильный
-                порывистый ветер. 25 октября.  Корабль за ночь разбило
-                в щепки; на том месте, где он стоял, торчат какие-то
-                жалкие обломки,  да и те видны только во время отлива.
-                Весь этот день я хлопотал  около вещей: укрывал и
-                укутывал их, чтобы не испортились от дождя.''',
-    },
-]
-
-# Для удобства собираем новую структуру
-new_posts = {post['id']: post for post in posts}
+from datetime import datetime
+from blog.models import Post, Category
+from django.shortcuts import render, get_object_or_404
 
 
 def index(request):
     """Функция обрабатывает ссылку на Главную страницу index.html."""
+    max_count_publish_posts = 5
     template_index = 'blog/index.html'
+    index_data = Post.objects.select_related(
+        'category'
+    ).filter(
+        pub_date__lte=datetime.now(),
+        is_published=True,
+        category__is_published=True
+    ).order_by('-id')[0:max_count_publish_posts]
 
-    # Сортируем по ключу
-    new_posts_desc = dict(sorted(new_posts.items(), reverse=True))
-    context = {'posts_list': new_posts_desc.values()}
+    context = {'post_list': index_data}
     return render(request, template_index, context)
 
 
 def post_detail(request, post_id):
     """Функция обрабатывает ссылку на Отдельный пост."""
     template_detail = 'blog/detail.html'
-    # Если не нашли выбрасываем исключение
-    if new_posts.get(post_id) is None:
-        raise Http404("Post does not exist")
-    context = {'post': new_posts[post_id]}
+    post_detail = get_object_or_404(
+        Post.objects.all().filter(
+            pub_date__lte=datetime.now(),
+            is_published=True,
+            category__is_published=True
+        ),
+        pk=post_id
+    )
+    context = {'post': post_detail}
     return render(request, template_detail, context)
 
 
 def category_posts(request, category_slug):
     """Функция обрабатывает ссылку на раздел Категории."""
     template_category = 'blog/category.html'
-    context = {'category': category_slug}
+
+    category = get_object_or_404(
+        Category, slug=category_slug, is_published=True
+    )
+
+    post_list = Post.objects.select_related(
+        'category'
+    ).filter(
+        is_published=True,
+        category__slug=category_slug,
+        pub_date__lte=datetime.now()
+    )
+
+    context = {'post_list': post_list,
+               'category': category}
     return render(request, template_category, context)
