fda0e97bd53951d5eaebe6b89f24b0294865cda6
Garry <biv171@yandex.ru>
2024-05-11 12:08:36 +0300

    django 2nd project improve models

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -1,24 +1,24 @@
-from datetime import datetime
+from django.db.models.functions import Now
 from django.shortcuts import get_object_or_404, render
 
 from blog.models import Category, Post
 
 # основные данные по постам
-posts = Post.objects.select_related(
-    'category', 'location'
+POSTS = Post.objects.select_related(
+    'category', 'location', 'author'
 ).filter(
     is_published=True,
-    category__is_published=True
+    category__is_published=True,
+    pub_date__lte=Now()
 )
-
-max_count_publish_posts = 5
+# кол-во постов выводимых на главную страницу
+COUNT_OF_POSTS = 5
 
 
 def index(request):
     """Функция обрабатывает ссылку на Главную страницу index.html."""
     template_index = 'blog/index.html'
-    index_data = posts.filter(
-        pub_date__lte=datetime.now())[:max_count_publish_posts]
+    index_data = POSTS[:COUNT_OF_POSTS]
     context = {'post_list': index_data}
     return render(request, template_index, context)
 
@@ -26,8 +26,7 @@ def index(request):
 def post_detail(request, post_id):
     """Функция обрабатывает ссылку на Отдельный пост."""
     template_detail = 'blog/detail.html'
-    post_detail = get_object_or_404(
-        posts.filter(pub_date__lte=datetime.now()), pk=post_id)
+    post_detail = get_object_or_404(POSTS, pk=post_id)
     context = {'post': post_detail}
     return render(request, template_detail, context)
 
@@ -40,10 +39,7 @@ def category_posts(request, category_slug):
         Category, slug=category_slug, is_published=True
     )
 
-    post_list = posts.filter(
-        category__slug=category_slug,
-        pub_date__lte=datetime.now()
-    )
+    post_list = POSTS.filter(category__slug=category_slug)
 
     context = {'post_list': post_list,
                'category': category}
